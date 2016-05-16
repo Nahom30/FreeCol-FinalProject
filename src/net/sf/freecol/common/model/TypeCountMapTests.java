c82a1565353c41ab5fb293e6d145c106fe9142c9
odomin2 <odomin2@students.towson.edu>
2016-05-16 18:42:58 -0400

    Added javadoc documentation to the TypeCountMapTests.java
    Added javadoc documentation and additional comments to follow standard
    javadoc comments practice.

--- src/net/sf/freecol/common/model/TypeCountMapTests.java
+++ src/net/sf/freecol/common/model/TypeCountMapTests.java
@@ -1,16 +1,44 @@
+/*
+ * Author that created the test cases was Kevat.
+ * Obed used javadoc tools to create documentation to test cases and added additional comments. 
+ */
 package net.sf.freecol.common.model;
 
 import junit.framework.TestCase;
 import static org.junit.Assert.*;
 import org.junit.*;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The class <code>TypeCountMapTests</code> contains test for the class <code> {@link TypeCountMap}</code>.
+ * The Class TypeCountMapTests.
+ *   
+ * @CommentEditor: Obed
+ * @author: Kevat
+ * 
+ */
 public class TypeCountMapTests {
+	
+	/** The map1. */
 	TypeCountMap<GoodsType> map1;
+	
+	/** The map2. */
 	TypeCountMap<GoodsType> map2;
+	
+	/** The type1. */
 	GoodsType type1;
+	
+	/** The type2. */
 	GoodsType type2;
+	
+	/** The type3. */
 	GoodsType type3;
 	
+	/**
+	 * Perform pre-test initialization 
+	 * 
+	 * Sets the setup.
+	 */
 	@Before
 	public void setUp() {
 		map1 =  new TypeCountMap<GoodsType>();
@@ -21,6 +49,13 @@ public class TypeCountMapTests {
 	}
 	
 	
+	/**
+	 * Run the void addToMap() method test. 
+	 * 
+	 * Adds the to map.
+	 * 
+	 * 
+	 */
 	@Test
 	public void addToMap() {
 		
@@ -33,6 +68,11 @@ public class TypeCountMapTests {
 		assertEquals(7, map1.getCount(type2));
 	}
 	
+	/**
+	 * Run the void removeFromMap() method test.  
+	 * 
+	 * Removes the from map.
+	 */
 	@Test
 	public void removeFromMap() {
 		map1.incrementCount(type1, 2);
@@ -41,6 +81,10 @@ public class TypeCountMapTests {
 		assertEquals(0, map1.getCount(type1));
 	}
 	
+	/**
+	 * Run the void addAnotherMap() method test.  
+	 * Adds the another map.
+	 */
 	@Test
 	public void addAnotherMap() {
 		map1.incrementCount(type1, 1);
@@ -60,6 +104,11 @@ public class TypeCountMapTests {
 	}
 
 
+	/**
+	 * Run the void clarMap() method test.  
+	 * 
+	 * Clear map.
+	 */
 	@Test
 	public void clearMap() {
 		map1.incrementCount(type1, 1);
@@ -74,6 +123,11 @@ public class TypeCountMapTests {
 		assertEquals(0, map1.getCount(type2));		
 	}
 	
+	/**
+	 * Run the void validateKeySet() method test.  
+	 * 
+	 * Validate key set.
+	 */
 	@Test
 	public void validateKeySet() {
 		map1.incrementCount(type1, 1);
@@ -82,6 +136,11 @@ public class TypeCountMapTests {
 		assertNotNull(map1.keySet());
 	}
 
+	/**
+	 * Run the void validateValues() method test. 
+	 * 
+	 * Validate values.
+	 */
 	@Test
 	public void validateValues() {
 		map1.incrementCount(type1, 1);
@@ -91,6 +150,11 @@ public class TypeCountMapTests {
 	}
 	
 
+	/**
+	 * Run the void validateContainsKey() method test. 
+	 * 
+	 * Validate contains key.
+	 */
 	@Test
 	public void validateContainsKey() {
 		map1.incrementCount(type1, 1);
@@ -101,6 +165,11 @@ public class TypeCountMapTests {
 		assertFalse(map1.containsKey(type3));
 	}
 	
+	/**
+	 * Run the void validateIsEmpty() method test. 
+	 * 
+	 * Validate is empty.
+	 */
 	@Test
 	public void validateIsEmpty() {
 		map1.incrementCount(type1, 1);
@@ -109,6 +178,11 @@ public class TypeCountMapTests {
 		assertTrue(map2.isEmpty());
 	}
 	
+	/**
+	 * Run the void validateSize() method test. 
+	 * 
+	 * Validate size.
+	 */
 	@Test
 	public void validateSize() {
 		map1.incrementCount(type1, 1);
@@ -117,6 +191,11 @@ public class TypeCountMapTests {
 		assertEquals(2, map1.size());
 	}
 	
+	/**
+	 * Run the void validateToString() method test. 
+	 * 
+	 * Validate to string.
+	 */
 	@Test
 	public void validateToString() {
 		map1.incrementCount(type1, 1);
@@ -125,6 +204,11 @@ public class TypeCountMapTests {
 		assertEquals("[net.sf.freecol.common.model.TypeCountMap [-1,1] [-1,5]]", map1.toString());
 	}
 	
+	/**
+	 * Perform post-test clean-up 
+	 * 
+	 * Tear down.
+	 */
 	@After
 	public void tearDown() {
 	    map1 = null;
