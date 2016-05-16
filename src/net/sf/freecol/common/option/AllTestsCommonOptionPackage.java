7d33534c52bdca944b562bdc27bb3b401792d4f1
odomin2 <odomin2@students.towson.edu>
2016-05-16 12:53:03 -0400

    Created another AllTests suite, but for the Common.Option package
    Created an AllTests suite for the Common Option package.  This test
    suite is for the coverage report of that package.

---
+++ src/net/sf/freecol/common/option/AllTestsCommonOptionPackage.java
@@ -0,0 +1,11 @@
+package net.sf.freecol.common.option;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ LanguageOptionTest.class, TextOptionTest.class })
+public class AllTestsCommonOptionPackage {
+
+}
