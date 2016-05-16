58ecb54abe5b65e5c215b7e36a306dd30b8fb43d
odomin2 <odomin2@students.towson.edu>
2016-05-16 19:17:06 -0400

    Added javadoc documentation to the EuropeWasTest.java
    Added javadoc documentation and additional comments to follow standard
    javadoc comments practice.

--- src/net/sf/freecol/common/model/EuropeWasTests.java
+++ src/net/sf/freecol/common/model/EuropeWasTests.java
@@ -1,3 +1,8 @@
+/*
+ * Author that created the test cases was Kevat.
+ * Obed used javadoc tools to create documentation to test cases and added additional comments.
+ * 
+ */
 package net.sf.freecol.common.model;
 
 import junit.framework.TestCase;
@@ -6,12 +11,34 @@ import net.sf.freecol.client.FreeColClient;
 import static org.junit.Assert.*;
 import org.junit.*;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The class <code>EuropeWasTests.java</code> contains tests for the class <code>{@link EuropeWas.java}</code>
+ * @CommentsEditor Obed
+ * @author Kevat 
+ * 
+ * The Class EuropeWasTests.
+ * 
+ */
 public class EuropeWasTests {
+	
+	/** The europe was1. */
 	EuropeWas europeWas1; 
+	
+	/** The europe1. */
 	Europe europe1;
+	
+	/** The game1. */
 	Game game1;
+	
+	/** The player1. */
 	Player player1;
 	
+	/**
+	 * Perform pre-test initialization. 
+	 * 
+	 * Sets the setUp.
+	 */
 	@Before
 	public void setUp() {
 		game1 = new Game();
@@ -20,18 +47,33 @@ public class EuropeWasTests {
 		europeWas1 = new EuropeWas(europe1);
 	}
 	
+	/**
+	 * Run the constructorTest() method test. 
+	 * 
+	 * Constructor test.
+	 */
 	@Test
 	public void constructorTest() {
 		assertNotNull(europeWas1);
 	}
 	
 
+	/**
+	 * Run the newUnitTest() method test.
+	 * 
+	 * New unit test.
+	 */
 	@Test
 	public void newUnitTest() {
 		//europe1
 		//europeWas1.getNewUnit();
 	}
 	
+	/**
+	 * Perform the post-test clean-up.
+	 * 
+	 * Tear down.
+	 */
 	@After
 	public void tearDown() {
 	
